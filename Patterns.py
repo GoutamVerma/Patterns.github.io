5a66e9a3028fcfde2c585f88ee5d1ea944da1926
Goutam Verma <66783850+GoutamVerma@users.noreply.github.com>
2020-07-04 21:46:58 +0530

    Create Patterns.py

---
+++ Patterns.py
@@ -0,0 +1,14 @@
+
+for x in range(1,6):
+    for y in range(5,x,-1):
+        print(" ", end="")
+    for x in range(1,x):
+        print("*",end="")
+    print()
+
+    
+#   *
+#  **
+# ***
+#****
+
